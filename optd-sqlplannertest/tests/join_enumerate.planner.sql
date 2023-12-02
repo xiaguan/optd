83036224dcb746a5f10105c750f0adeb4f33e127
Alex Chi <iskyzh@gmail.com>
2023-12-02 15:17:50 -0500

    test: adjust join enumerate test
    Signed-off-by: Alex Chi <iskyzh@gmail.com>

--- optd-sqlplannertest/tests/join_enumerate.planner.sql
+++ optd-sqlplannertest/tests/join_enumerate.planner.sql
@@ -1,10 +1,10 @@
 -- (no id or description)
 create table t1(t1v1 int, t1v2 int);
-create table t2(t2v1 int);
-create table t3(t3v2 int);
+create table t2(t2v1 int, t2v3 int);
+create table t3(t3v2 int, t3v4 int);
 insert into t1 values (0, 0), (1, 1), (2, 2);
-insert into t2 values (0), (1), (2);
-insert into t3 values (0), (1), (2);
+insert into t2 values (0, 200), (1, 201), (2, 202);
+insert into t3 values (0, 300), (1, 301), (2, 302);
 
 /*
 3
@@ -16,14 +16,18 @@ insert into t3 values (0), (1), (2);
 select * from t2, t1, t3 where t1v1 = t2v1 and t1v2 = t3v2;
 
 /*
+(Join (Join t2 t1) t3)
+(Join t2 (Join t1 t3))
 (Join (Join t1 t3) t2)
 (Join t3 (Join t1 t2))
 (Join t3 (Join t2 t1))
-(Join t2 (Join t3 t1))
-(Join t2 (Join t1 t3))
-(Join (Join t1 t2) t3)
 (Join (Join t3 t1) t2)
-(Join (Join t2 t1) t3)
+(Join (Join t1 t2) t3)
+(Join t2 (Join t3 t1))
+
+0 200 0 0 0 300
+1 201 1 1 1 301
+2 202 2 2 2 302
 */
 
 -- Test whether the optimizer enumerates all join orders.
@@ -31,12 +35,16 @@ select * from t1, t2, t3 where t1v1 = t2v1 and t1v2 = t3v2;
 
 /*
 (Join t3 (Join t1 t2))
-(Join t2 (Join t1 t3))
-(Join t3 (Join t2 t1))
 (Join (Join t2 t1) t3)
-(Join (Join t1 t3) t2)
 (Join (Join t1 t2) t3)
+(Join t2 (Join t1 t3))
 (Join t2 (Join t3 t1))
+(Join (Join t1 t3) t2)
+(Join t3 (Join t2 t1))
 (Join (Join t3 t1) t2)
+
+0 0 0 200 0 300
+1 1 1 201 1 301
+2 2 2 202 2 302
 */
 
